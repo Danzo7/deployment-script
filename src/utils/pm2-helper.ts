dddd4c067fef428e88b09dc7b6f1a44e688c1160
danzo7 <aymenmiro@gmail.com>
2024-12-04 23:31:46 +0100

    🐛 Fix issue

--- src/utils/pm2-helper.ts
+++ src/utils/pm2-helper.ts
@@ -10,65 +10,57 @@ export const runApp = async (dir: string,config: Omit<pm2.StartOptions,"exec_mod
           return reject(err);
         }
   
-        // Check if the app is already running
-        pm2.list((listErr, processList) => {
-          if (listErr) {
+        if (config.status=="online") {
+          // If the app is found, restart it
+          pm2.restart(config.name, (restartErr) => {
             pm2.disconnect();
-            return reject(listErr);
-          }
-    
-          if (config.status=="online") {
-            // If the app is found, restart it
-            pm2.restart(config.name, (restartErr) => {
-              pm2.disconnect();
-              if (restartErr) {
-                return reject(restartErr);
-              }
-  
-              Logger.info(`PM2 process "${config.name}" restarted successfully.`);
-              resolve();
-            });
-          } else {
-            // If the app is not found, start it
-            if(config.status=="not-found")
-            pm2.start({...config,
-              exec_mode: "cluster",
-              cwd:dir,  
-              script: `node_modules/next/dist/bin/next`,
-              args: `start -p ${config.port}`,
-              max_memory_restart: "250M",
-            }, (startErr) => {
-              pm2.disconnect();
-              if (startErr) {
-                return reject(startErr);
-              }
-              Logger.info(`PM2 process "${config.name}" started successfully.`);
-              resolve();
-            });
-            else{
-              pm2.delete(config.name, (deleteErr) => {
-                  if(deleteErr){
-                    pm2.disconnect();  
-                    return reject(deleteErr);   
-                  } 
-                  pm2.start({...config,
-                    exec_mode: "cluster",
-                    cwd:dir,  
-                    script: `node_modules/next/dist/bin/next`,
-                          args: `start -p ${config.port}`,
-                    max_memory_restart: "250M",
-                  }, (startErr) => {
-                    pm2.disconnect();
-                    if (startErr) {
-                      return reject(startErr);
-                    }
-                    Logger.info(`PM2 process "${config.name}" started successfully.`);
-                    resolve();
-                  });
-              })
+            if (restartErr) {
+              return reject(restartErr);
             }
+
+            Logger.info(`PM2 process "${config.name}" restarted successfully.`);
+            resolve();
+          });
+        } else {
+          // If the app is not found, start it
+          if(config.status=="not-found")
+          pm2.start({...config,
+            exec_mode: "cluster",
+            cwd:dir,  
+            script: `node_modules/next/dist/bin/next`,
+            args: `start -p ${config.port}`,
+            max_memory_restart: "250M",
+          }, (startErr) => {
+            pm2.disconnect();
+            if (startErr) {
+              return reject(startErr);
+            }
+            Logger.info(`PM2 process "${config.name}" started successfully.`);
+            resolve();
+          });
+          else{
+            pm2.delete(config.name, (deleteErr) => {
+                if(deleteErr){
+                  pm2.disconnect();  
+                  return reject(deleteErr);   
+                } 
+                pm2.start({...config,
+                  exec_mode: "cluster",
+                  cwd:dir,  
+                  script: `node_modules/next/dist/bin/next`,
+                  args: `start -p ${config.port}`,
+                  max_memory_restart: "250M",
+                }, (startErr) => {
+                  pm2.disconnect();
+                  if (startErr) {
+                    return reject(startErr);
+                  }
+                  Logger.info(`PM2 process "${config.name}" started successfully.`);
+                  resolve();
+                });
+            })
           }
-        });
+        }
       });
     });
   };
