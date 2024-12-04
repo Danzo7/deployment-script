c7fb8cd4cbe8242595a7db828b6a6b84df35054e
danzo7 <aymenmiro@gmail.com>
2024-12-04 22:57:20 +0100

    🐛 fix issue

--- src/utils/pm2-helper.ts
+++ src/utils/pm2-helper.ts
@@ -1,8 +1,9 @@
 import pm2, { ProcessDescription } from "pm2";
 import { Logger } from "./logger.js";
 import path from "path";
+type Status='online' | 'stopping' | 'stopped' | 'launching' | 'errored' | 'one-launch-status'|'not-found';
 
-export const runApp = async (dir: string,config: Omit<pm2.StartOptions,"exec_mode"|"script"|"args">&{name:string,port:number}) => {
+export const runApp = async (dir: string,config: Omit<pm2.StartOptions,"exec_mode"|"script"|"args">&{name:string,port:number,status:Status}) => {
     return new Promise<void>((resolve, reject) => {
       pm2.connect((err) => {
         if (err) {
@@ -15,10 +16,8 @@ export const runApp = async (dir: string,config: Omit<pm2.StartOptions,"exec_mod
             pm2.disconnect();
             return reject(listErr);
           }
-  
-          const existingApp = processList.find((app) => app.name === config.name);
-  
-          if (existingApp) {
+    
+          if (config.status=="online") {
             // If the app is found, restart it
             pm2.restart(config.name, (restartErr) => {
               pm2.disconnect();
@@ -30,7 +29,8 @@ export const runApp = async (dir: string,config: Omit<pm2.StartOptions,"exec_mod
               resolve();
             });
           } else {
-            // If the app is not running, start it
+            // If the app is not found, start it
+            if(config.status=="not-found")
             pm2.start({...config,
               exec_mode: "cluster",
               script: path.join(dir, `node_modules/next/dist/bin/next`),
@@ -44,12 +44,32 @@ export const runApp = async (dir: string,config: Omit<pm2.StartOptions,"exec_mod
               Logger.info(`PM2 process "${config.name}" started successfully.`);
               resolve();
             });
+            else{
+              pm2.delete(config.name, (deleteErr) => {
+                  if(deleteErr){
+                    pm2.disconnect();  
+                    return reject(deleteErr);   
+                  } 
+                  pm2.start({...config,
+                    exec_mode: "cluster",
+                    script: path.join(dir, `node_modules/next/dist/bin/next`),
+                    args: `start -p ${config.port}`,
+                    max_memory_restart: "250M",
+                  }, (startErr) => {
+                    pm2.disconnect();
+                    if (startErr) {
+                      return reject(startErr);
+                    }
+                    Logger.info(`PM2 process "${config.name}" started successfully.`);
+                    resolve();
+                  });
+              })
+            }
           }
         });
       });
     });
   };
-  type Status='online' | 'stopping' | 'stopped' | 'launching' | 'errored' | 'one-launch-status'|'not found';
 
   export const  getAppStatus=async(name:string)=>new Promise<Status>((resolve, reject) => {
     pm2.connect((err) => {
@@ -67,10 +87,10 @@ export const runApp = async (dir: string,config: Omit<pm2.StartOptions,"exec_mod
         const app = processList.find((app) => app.name === name);
         if(app)
           resolve(
-            app.pm2_env?.status??"not found"
+            app.pm2_env?.status??"not-found"
           )
           else resolve(
-            "not found"
+            "not-found"
           );
       
   })})});
