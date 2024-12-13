6f064f628db3879b21b59494d173b48dbbd1a025
danzo7 <aymenmiro@gmail.com>
2024-12-13 19:15:47 +0100

    🎨 🚑 radust pm2 config

--- src/utils/pm2-helper.ts
+++ src/utils/pm2-helper.ts
@@ -74,7 +74,6 @@ export const runApp = async (
                 script: `node_modules/next/dist/bin/next`,
                 args: `start -p ${config.port}`,
                 max_memory_restart: '250M',
-                watch:true,
                 restart_delay:100
 
               },
