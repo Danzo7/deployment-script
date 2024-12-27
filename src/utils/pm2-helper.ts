59d680127dd5aecb1804c6bdb9d15e947c8f10df
danzo7 <aymenmiro@gmail.com>
2024-12-27 18:03:20 +0100

    ⚡️ Add build path to next run

--- src/utils/pm2-helper.ts
+++ src/utils/pm2-helper.ts
@@ -22,7 +22,7 @@ export const runApp = async (
     exec_mode: 'cluster',
     cwd: dir,
     script: `node_modules/next/dist/bin/next`,
-    args: `start -p ${port}`,
+    args: `start ${dir} -p ${port}`,
     max_memory_restart: '250M',
 
   };
